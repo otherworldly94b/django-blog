7740e237a3de1034dcb7d23a427aa6b04cb6c06a
otherworldly94b <and.charalambous@gmail.com>
2024-02-19 18:31:23 +0000

    Redeploy with DEBUG = False

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-otherworldly-djangoblog-2kzhixmqiya.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-otherworldly-djangoblog-14rkigjnp0p.ws-eu108.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
