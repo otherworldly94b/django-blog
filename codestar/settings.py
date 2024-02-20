29c64a3d4bb99d75c7b0a0e2b1e234fed06425e2
otherworldly94b <and.charalambous@gmail.com>
2024-02-20 18:00:22 +0000

    Create about app and link to everythiing it needs to link

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-otherworldly-djangoblog-14rkigjnp0p.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'django_summernote',
+    'about',
 ]
 
 MIDDLEWARE = [
